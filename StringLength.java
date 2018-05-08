833be88b54bc7fa9c29a35033a30d9b33679ec7d
Rachael Young <rly123@MW-MBP.local>
2018-05-08 16:12:18 -0600

    StringLength fix

--- StringLength.java
+++ StringLength.java
@@ -1,19 +1,30 @@
-
+import java.util.*;
 public class StringLength {
 
 
     public static void main(String[] args) {
         int numChars=1;
-        String test="l";
+        ArrayList<String> test=new ArrayList<String>();
         try {
             while(true)
             {
-                test+="llllllllllllllllllllllllllllllllllllll";
-                numChars+=40;
+                test.add(getRandom());
+                numChars+=10;
             }
         } catch (OutOfMemoryError E) {
             System.out.println(numChars);
         }
+
+    }
+    private static String getRandom() {
+        String possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder randString = new StringBuilder();
+        Random rnd = new Random();
+        while (randString.length() < 10) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * possible.length());
+            randString.append(possible.charAt(index));
+        }
+        return randString.toString();
 
     }
 }
